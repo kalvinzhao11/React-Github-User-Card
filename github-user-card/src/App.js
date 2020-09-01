9a077c23b3091f6f42127fbf6fe28b1fc516b490
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-09-01 19:08:20 -0400

    add the ability to search users

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import GitHubCalendar from 'github-calendar'
 import './App.css';
 
 
 class App extends Component {
   state = {
     user: '',
-    username: '',
+    username: 'kalvinzhao11',
     followers: '',
-    error: ''
+    error: '',
+    search: '',
   }
   componentDidMount(){
     axios
       .get('https://api.github.com/users/kalvinzhao11')
       .then(response => {
         this.setState({user: response.data})
+        this.setState({error: ''})
       })
       .catch(error => {
         this.setState({error: 'This user does not exist'})
@@ -23,9 +26,10 @@ class App extends Component {
       .get('https://api.github.com/users/kalvinzhao11/followers')
       .then(response => {
         this.setState({followers: response.data})
+        this.setState({error: ''})
       })
       .catch(error => {
-        // this.setState({error: 'This user does not exist'})
+        this.setState({error: 'This user does not exist'})
       })
 
   }
@@ -36,31 +40,54 @@ class App extends Component {
         .get(`https://api.github.com/users/${this.state.username}`)
         .then(response => {
           this.setState({user: response.data})
-          console.log(this.state.user)
+          this.setState({error: ''})
         })
         .catch(error => {
-          this.setState({error: 'This username does not exist'})
+          this.setState({error: 'This user does not exist'})
+        })
+      axios
+        .get(`https://api.github.com/users/${this.state.username}/followers`)
+        .then(response => {
+          this.setState({followers: response.data})
+          this.setState({error: ''})
+        })
+        .catch(error => {
+          this.setState({error: 'This user does not exist'})
         })
     }
-    axios
-    .get(`https://api.github.com/users/${this.state.username}/followers`)
-    .then(response => {
-      this.setState({followers: response.data})
-    })
-    .catch(error => {
-      // this.setState({error: 'This user does not exist'})
-    })
   }
 
   onClick = (event) => {
     this.setState({username: event.target.innerText})
-    console.log(event.target.value)
   }
+
+  onChange = (event) => {
+    this.setState({search: event.target.value})
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      username: this.state.search,
+      search: ''
+    })
+  }
+
+  // GitHubCalendar(".calendar", "kalvinzhao11");
+
   render() {
-    console.log(this.state.user)
     return (
     <div class="card">
       <p>Github Card</p>
+      <form onSubmit={this.onSubmit}>
+        <label>Search Github User: </label>
+        <input
+          value={this.state.search}
+          onChange={this.onChange}
+        ></input>
+        <button>Search</button>
+      </form>
+      { this.state.error ? <p>{this.state.error}</p> : <p></p>}
       <img src={this.state.user.avatar_url} />
       <div class="card-info">
         <h3 class="name">{this.state.user.name}</h3>
@@ -70,7 +97,6 @@ class App extends Component {
           <a href={this.state.user.html_url}>{this.state.user.html_url}</a>
         </p>
         <p>Followers: {this.state.user.followers}</p>
-        {console.log(this.state.followers)}
         { 
           this.state.followers ? this.state.followers.map(follower => (
              <p key={follower.id} value={follower.login} onClick={this.onClick}>{follower.login}</p>
@@ -79,6 +105,10 @@ class App extends Component {
         <p>Following: {this.state.user.following}</p>
         <p>Bio: {this.state.user.bio}</p>
       </div>
+
+      <div class="calendar">
+      </div>
+
     </div>
     )
   }
