8952004f0480fe3839df72443d6f4d0ed339d434
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-09-01 21:07:59 -0400

    added basic styling

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import GitHubCalendar from 'github-calendar'
+import { Button, Input } from 'reactstrap';
+import styled from 'styled-components'
 import './App.css';
 
+const Div = styled.div `
+  text-align: center;
+`
+
+const P = styled.p `
+  text-decoration: underline;
+  color: red;
+`
 
 class App extends Component {
   state = {
@@ -77,39 +87,38 @@ class App extends Component {
     GitHubCalendar(".calendar", `${this.state.username}`);
 
     return (
-    <div class="card">
-      <p>Github Card</p>
+    <Div class="card">
       <form onSubmit={this.onSubmit}>
         <label>Search Github User: </label>
-        <input
+        <Input
           value={this.state.search}
           onChange={this.onChange}
-        ></input>
-        <button>Search</button>
+        ></Input>
+        <Button outline color="warning">Search</Button>
       </form>
       { this.state.error ? <p>{this.state.error}</p> : <p></p>}
       <img src={this.state.user.avatar_url} />
       <div class="card-info">
         <h3 class="name">{this.state.user.name}</h3>
         <p class="username">{this.state.user.login}</p>
-        <p>Location: {this.state.user.location}</p>
+        <p>Location: {this.state.user.location ? this.state.user.location : 'N/A'}</p>
         <p>Profile:
           <a href={this.state.user.html_url}>{this.state.user.html_url}</a>
         </p>
         <p>Followers: {this.state.user.followers}</p>
         { 
           this.state.followers ? this.state.followers.map(follower => (
-             <p key={follower.id} value={follower.login} onClick={this.onClick}>{follower.login}</p>
+             <P key={follower.id} value={follower.login} onClick={this.onClick}>{follower.login}</P>
           )) : ''
         }
         <p>Following: {this.state.user.following}</p>
-        <p>Bio: {this.state.user.bio}</p>
+        <p>Bio: {this.state.user.bio ? this.state.user.bio : 'N/A'}</p>
       </div>
 
       <div class="calendar">
       </div>
 
-    </div>
+    </Div>
     )
   }
 }
